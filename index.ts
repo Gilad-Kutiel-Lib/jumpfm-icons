2068c6efe1d583ddb2681ad39cd0e4a4832c0b32
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-25 18:18:22 +0300

    icons

---
+++ index.ts
@@ -0,0 +1,31 @@
+import { JumpFm, Item } from 'jumpfm-api'
+
+import * as fs from 'fs'
+import * as path from 'path'
+
+const icons = {}
+const exts = require('./icons.json') as { [icon: string]: string[] }
+Object.keys(exts).forEach(icon => {
+    exts[icon].forEach(ext => icons[ext] = icon)
+})
+
+const getIcon = (item: Item) => {
+    const icon = icons[path.extname(item.name).substr(1)]
+    if (icon) return `file_type_${icon}.svg`
+    return fs.statSync(item.path).isDirectory()
+        ? 'default_folder.svg'
+        : 'default_file.svg'
+}
+
+export const load = (jumpFm: JumpFm) => {
+
+
+    jumpFm.panels.forEach(panel => {
+        panel.onItemsAdded(newItems => {
+            newItems.forEach(item => {
+                const icon = getIcon(item)
+                item.setIcon(path.join(__dirname, 'icons', icon))
+            })
+        })
+    })
+}
